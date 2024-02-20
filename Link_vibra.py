d34479016928e61d778abf3d539fee1244f4f582
BerMud4X <122708563+BerMud4X@users.noreply.github.com>
2024-02-20 20:47:50 +0100

    Add files via upload

---
+++ Link_vibra.py
@@ -0,0 +1,13 @@
+import serial
+import keyboard
+import time
+
+# Configure the serial port
+ser = serial.Serial('COMX', 9600)  # Replace 'COMX' with your Arduino's serial port
+
+while True:
+    if keyboard.is_pressed('a'):
+        ser.write(b'A')  # Send 'A' to Arduino
+    elif keyboard.is_pressed('p'):
+        ser.write(b'P')  # Send 'P' to Arduino
+    time.sleep(0.5)
